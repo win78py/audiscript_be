1843b08dd8a7d6933b057e33fbd42729dd621658
win78py <thangtnsa000@gmail.com>
2025-07-24 14:57:28 +0700

    fix timeout

--- main.go
+++ main.go
@@ -65,7 +65,7 @@ func main() {
 		Addr:         fmt.Sprintf(":%s", port),
 		Handler:      r,
 		ReadTimeout:  20 * time.Second,
-		WriteTimeout: 60 * time.Second,
+		WriteTimeout: 900 * time.Second,
 		IdleTimeout:  time.Minute,
 	}
 
