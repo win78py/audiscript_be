db0a55307b3cfbe414903b54f7d60080cf1f74f7
win78py <thangtnsa000@gmail.com>
2025-08-03 19:53:07 +0700

    add fields in User

--- internal/auth/auth_model.go
+++ internal/auth/auth_model.go
@@ -1,25 +1,29 @@
 package auth
 
 import (
-    "time"
-    "gorm.io/gorm"
+	"gorm.io/gorm"
+	"time"
 )
 
 // User represents application user
 type User struct {
-    ID        uint           `gorm:"primaryKey" json:"id"`
-    Email     string         `gorm:"unique;not null" json:"email"`
-    Password  string         `gorm:"not null" json:"-"`
-    CreatedAt time.Time      `json:"createdAt"`
-    UpdatedAt time.Time      `json:"updatedAt"`
-    DeletedAt gorm.DeletedAt `gorm:"index" json:"-"`
+	ID           uint           `gorm:"primaryKey" json:"id"`
+	Username     string         `json:"username"`
+	FirstName    string         `json:"firstName"`
+	LastName     string         `json:"lastName"`
+	ProfileImage string         `gorm:"default:'https://res.cloudinary.com/dj2krdujc/image/upload/v1754225216/audiscript/profile/Rose-Blackpink_trxiwz.jpg'" json:"profileImage"`
+	Email        string         `gorm:"unique;not null" json:"email"`
+	Password     string         `gorm:"not null" json:"-"`
+	CreatedAt    time.Time      `json:"createdAt"`
+	UpdatedAt    time.Time      `json:"updatedAt"`
+	DeletedAt    gorm.DeletedAt `gorm:"index" json:"-"`
 }
 
 // RefreshToken to support token revocation
 type RefreshToken struct {
-    ID        uint      `gorm:"primaryKey"`
-    Token     string    `gorm:"unique;not null"`
-    UserID    uint      `gorm:"index;not null"`
-    ExpiresAt time.Time `gorm:"not null"`
-    CreatedAt time.Time
+	ID        uint      `gorm:"primaryKey"`
+	Token     string    `gorm:"unique;not null"`
+	UserID    uint      `gorm:"index;not null"`
+	ExpiresAt time.Time `gorm:"not null"`
+	CreatedAt time.Time
 }
